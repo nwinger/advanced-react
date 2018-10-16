7f952c3c7b7ec95f053aaf38644150fdd52c6b93
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-10-16 20:05:38 +0200

    Complete video 14 - Items creation and Prisma Yoga flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,24 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    // Create a dog
-    const newDog = { name: args.name };
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-    global.dogs.push(newDog);
-    return newDog;
+    return item;
   }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // Create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
